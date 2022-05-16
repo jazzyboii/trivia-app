6426894fe5f4c9d7478046b92963a1edd2426812
David Vincent <70216205+jazzyboii@users.noreply.github.com>
2022-05-16 16:21:57 -0400

    trivia score progression

--- trivia-app/src/App.jsx
+++ trivia-app/src/App.jsx
@@ -1,55 +1,54 @@
 import React from 'react';
-import {Questions}  from './Questions';
+import Questions  from './Questions';
 import {useEffect,useState} from "react";
 import {Typography, Divider, AppBar, Button,CssBaseline,Toolbar} from '@mui/material';
-
-
-function App() {
-  const [triviaData, setTriviaData] = useState();
-  //const [loading, setLoading] = useState();
-  //const [totalAnswered, setTotalAnswered] = useState(0);
-  //const [totalCorrect, setTotalCorrect] = useState(0);
-  const newQuestions = () => {
-    fetch("https://opentdb.com/api.php?amount=10&category=11&type=multiple")
-    .then((res) => res.json())
-    .then((data) => setTriviaData(data.results))
+  // const [triviaData, setTriviaData] = useState();
+  // const newQuestions = () => {
+  //   fetch("https://opentdb.com/api.php?amount=10&category=11&type=multiple")
+  //   .then((res) => res.json())
+  //   .then((data) => setTriviaData(data.results))
      
-  }
-  useEffect(() => {
-    newQuestions();
-  }, [])
+  // }
+  // useEffect(() => {
+  //   newQuestions();
+  // }, [])
 
 
-  if (!triviaData) 
-  {
-      return (
-        <div>
-          <h1> Pleses wait some time.... </h1> 
-        </div>
-      );
-  }else{
-      return (
-        <>
-          <CssBaseline/>
-            <AppBar position="relative">
-            <Toolbar>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                Trivia app | Score:
-              </Typography>
-              <Button color="inherit">New Questions</Button>       
-            </Toolbar>
-          </AppBar>
-          <main>
-            <div>
-                <Questions/>
-            </div>
-            <Button variant="contained" align="center"> Submit </Button>      
-            <Divider />  
-          </main>
-        </>
-      );
-  }
+class App extends React.Component{
 
+    render(){
+        if (!true) 
+        {
+            return (
+              <div>
+                <h1> Please wait some time.... </h1> 
+              </div>
+            );
+        }else{
+            return (
+              <>
+                <CssBaseline/>
+                  <AppBar position="sticky">
+                    <Toolbar>
+                      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Trivia app | Score: {0}
+                      </Typography>
+                      <Button color="inherit">New Questions</Button>       
+                    </Toolbar>
+                  </AppBar>
+                <main>
+                  <div>
+                      <Questions/>
+                  </div>
+                  <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '10vh'}}>
+                    <Button variant="contained" align="center"> Submit </Button> 
+                  </div>
+                  <Divider />  
+                </main>
+              </>
+            );
+        }      
+    }
 }
 
 export default App;
--- trivia-app/src/Questions.js
+++ trivia-app/src/Questions.js
@@ -1,74 +1,97 @@
 import React from 'react';
 import {triviaData} from "./trivia";
 import {Typography, List, ListItem, ListItemText, Divider,Container} from '@mui/material';
+import {decode} from 'html-entities';
+
+
+class Questions extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            score: 0
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        
+        this.setState({
+            score: this.state.score + 1
+        })
+        console.log(this.state.score)
+    }
+    render(){
+
+        const Question = ({question, correct_answer, incorrect_answer1,incorrect_answer2, incorrect_answer3}) => {
+            
+            return (
+                <>
+                    <Container maxWidth="sm">
+                        <Typography variant="h6" align="left" color="textPrimary" gutterBottom>
+                            {decode(question)}
+                        </Typography>
+                        <Answer
+                            correct_answer={correct_answer}
+                            incorrect_answer1={incorrect_answer1}
+                            incorrect_answer2={incorrect_answer2}
+                            incorrect_answer3={incorrect_answer3}
+                        />
+                    </Container>
+                    <Divider />      
+                </>
+            );
+        };
+        const Answer = ({correct_answer, incorrect_answer1,incorrect_answer2, incorrect_answer3}) => {
+            
+            return (
+                <>
+                    <List component="nav" aria-label="mailbox folders">
+                    <ListItem button onClick={this.handleClick} selected={true}>
+                        <ListItemText primary={decode(correct_answer)} />
+                    </ListItem>
+                    <Divider />
+                    <ListItem button divider>
+                        <ListItemText primary={decode(incorrect_answer1)} />
+                    </ListItem>
+                    <ListItem button >
+                        <ListItemText primary={decode(incorrect_answer2)} />
+                    </ListItem>
+                    <Divider light />
+                    <ListItem button>
+                        <ListItemText primary={decode(incorrect_answer3)} />
+                    </ListItem>
+                    </List>     
+                </>
+            );
+        };
+
+        return (
+            <>
+                <div>        
+                    {triviaData.map((data,key) => {
+                        return (
+                            <div key={key}>
+                                <Question
+                                    key={key}
+                                    question={data.question}
+                                    correct_answer={data.correct_answer}
+                                    incorrect_answer1={data.incorrect_answers[0]}
+                                    incorrect_answer2={data.incorrect_answers[1]}
+                                    incorrect_answer3={data.incorrect_answers[2]}
+                                />
+                            </div>
+                        );
+                    })}
+                </div>
+            </>            
+        );
+    }
+}
 
 
 
-export const Questions = () => {
 
-      return (
-        <>
-          <div>        
-            {triviaData.map((data,key) => {
-                return (
-                    <div key={key}>
-                        <>
-                            <Container maxWidth="sm">
-                                <Typography variant="h6" align="left" color="textPrimary" gutterBottom>
-                                    {data.question}
-                                </Typography>
-                                <List component="nav" aria-label="mailbox folders">
-                                <ListItem button selected="true">
-                                    <ListItemText primary={data.correct_answer} />
-                                </ListItem>
-                                <Divider />
-                                <ListItem button divider>
-                                    <ListItemText primary={data.incorrect_answers[0]} />
-                                </ListItem>
-                                <ListItem button>
-                                    <ListItemText primary={data.incorrect_answers[1]} />
-                                </ListItem>
-                                <Divider light />
-                                <ListItem button>
-                                    <ListItemText primary={data.incorrect_answers[2]} />
-                                </ListItem>
-                                </List>
-                            </Container>
-                            <Divider />      
-                       </>
-                    </div>
-                );
-            })}
-          </div>
-        </>
-      );
-}; 
 
-const Question = ({ question, correct_answer, incorrect_answer1,incorrect_answer2, incorrect_answer3}) => {
-    return (
-        <>
-        <Container maxWidth="sm">
-            <Typography variant="h6" align="left" color="textPrimary" gutterBottom>
-                {question}
-            </Typography>
-            <List component="nav" aria-label="mailbox folders">
-            <ListItem button selected="true">
-                <ListItemText primary={correct_answer} />
-            </ListItem>
-            <Divider />
-            <ListItem button divider>
-                <ListItemText primary={incorrect_answer1} />
-            </ListItem>
-            <ListItem button>
-                <ListItemText primary={incorrect_answer2} />
-            </ListItem>
-            <Divider light />
-            <ListItem button>
-                <ListItemText primary={incorrect_answer3} />
-            </ListItem>
-            </List>
-        </Container>
-        <Divider />      
-        </>
-    );
-  };
+
+export default Questions;
+
